baac6ee48b7b077356e1cb60b5fb096cfccff132
Zhiyuan Chen <quic_zhiyuanc@quicinc.com>
2023-04-07 15:28:50 +0530

    video: driver: Add VP8 Codec info for kona
    Add VP8 Codec information for kona target
    
    Change-Id: I5a0f82d83beeb7a0580afd24c90d738657384363

--- msm/vidc/msm_vidc_buffer_calculations.c
+++ msm/vidc/msm_vidc_buffer_calculations.c
@@ -479,6 +479,10 @@ int msm_vidc_get_num_ref_frames(struct msm_vidc_inst *inst)
 	struct v4l2_ctrl *max_layer = NULL;
 	u32 codec;
 
+	codec = get_v4l2_codec(inst);
+	if (codec == V4L2_PIX_FMT_VP8)
+		num_ref = num_ref << 1;
+
 	bframe_ctrl = get_ctrl(inst, V4L2_CID_MPEG_VIDEO_B_FRAMES);
 	num_bframes = bframe_ctrl->val;
 	if (num_bframes > 0)
@@ -495,7 +499,6 @@ int msm_vidc_get_num_ref_frames(struct msm_vidc_inst *inst)
 		V4L2_CID_MPEG_VIDC_VIDEO_HEVC_MAX_HIER_CODING_LAYER);
 	if (frame_t->val == V4L2_MPEG_VIDEO_HEVC_HIERARCHICAL_CODING_P &&
 		max_layer->val > 1) {
-		codec = get_v4l2_codec(inst);
 		/* LTR and B - frame not supported with hybrid HP */
 		if (inst->hybrid_hp)
 			num_ref = (max_layer->val + 1) >> 1;
