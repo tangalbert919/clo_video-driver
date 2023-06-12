716c0b84bca3dc32f6da95d659b4f0b7808a800d
Paras Nagda <quic_pnagda@quicinc.com>
2023-06-12 12:50:54 +0530

    video: driver: Enable video driver for compilation
    - Remove mmrm dependency
    - Enable techpack flag for vidc
    
    Change-Id: I345c4dda124cc7e83e332250b8520a7cc9ab376a
    Signed-off-by: Paras Nagda <quic_pnagda@quicinc.com>
    (cherry picked from commit 2fb0e7f9e225f315edfbe54f6cda0c064d908119)

--- video_kernel_board.mk
+++ video_kernel_board.mk
@@ -10,7 +10,7 @@ endif
 
 # Build video kernel driver
 ifeq ($(TARGET_VIDC_ENABLE),true)
-ifneq ($(TARGET_BOARD_AUTO),true)
+ifeq ($(TARGET_BOARD_AUTO),true)
 ifeq ($(call is-board-platform-in-list,$(TARGET_BOARD_PLATFORM)),true)
 BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/msm_video.ko
 
